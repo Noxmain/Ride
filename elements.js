0c4e5da9bb57812f8255f1294ca246a65a797782
Noxmain <noah.brandt@me.com>
2024-01-19 16:32:27 +0100

    Added world

--- elements.js
+++ elements.js
@@ -1,40 +1,56 @@
-function distance(a, b) {
-    return dist(a.x, a.y, b.x, b.y);
-}
+let planet_chance = 1 / 100;
+let ufo_chance = 1 / 600;
+let item_chance = 1 / 1000;
 
-function Element(x, y, r) {
+let elements = [];
+function planets() {return elements.filter(x => x.t == "planet");}
+function ufos() {return elements.filter(x => x.t == "ufo");}
+function items() {return elements.filter(x => x.t == "item");}
+function obstacles() {return planets().concat(ufos());}
+function collide(thing, things) {for (let t of things) {if (dist(t.x, t.y, thing.x, thing.y) <= t.r + thing.r) {return t;}}}
+
+function Element(t, x, y, r) {
+    this.t = t;
     this.x = x;
     this.y = y;
     this.r = r;
     this.xs = 0;
     this.ys = 1;
-    this.icon = icons.player;
+    this.img = img.player;
 
     this.update = function() {
         this.x += this.xs * speed;
         this.y += this.ys * speed;
+
+        if (this.xs != 0) {
+            if (collide(this, planets())) {this.xs *= -1;}
+            if (this.x + this.r >= width) {this.xs = -1;}
+            if (this.x - this.r <= 0) {this.xs = 1;}
+        }
     };
 
     this.visible = function() {
-        return this.y - this.r < world.height;
-    };
-
-    this.collide = function(other) {
-        return distance(this, other) <= this.r + other.r;
+        return this.y - this.r < height;
     };
 
     this.draw = function() {
-        image(this.icon, this.x - this.r, this.y - this.r, this.r * 2, this.r * 2);
+        image(this.img, this.x - this.r, this.y - this.r, this.r * 2, this.r * 2);
+        if (HITBOXES) {circle(this.x, this.y, this.r * 2);}
     };
 }
 
 function Planet(x, r) {
-    Element.call(this, x, -r, r);
-    this.icon = random(icons.planets);
+    Element.call(this, "planet", x, -r, r);
+    this.img = random(img.planets);
 }
 
-function Ufo(x, y, r) {
-    Element.call(this, x, y, r);
+function Ufo(x, y) {
+    Element.call(this, "ufo", x, y, 20);
     this.xs = random([1, -1]);
-    this.icon = icons.ufo;
+    this.img = img.ufo;
+}
+
+function Item(x, t) {
+    Element.call(this, "item", x, -10, 10);
+    this.img = img[t]
 }
--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
-let icons = {};
-let elements = [];
-let speed = 1;
+let HITBOXES = false;
+let speed = 5;
 
+let img = {};
 function preload() {
-    icons.menu = loadImage("img/menu.png");
-    icons.over = loadImage("img/over.png");
-    icons.background = loadImage("img/background.png");
+    img.menu = loadImage("img/menu.png");
+    img.over = loadImage("img/over.png");
+    img.background = loadImage("img/background.png");
 
-    icons.player = loadImage("img/rocket.png");
-    icons.planets = [
+    img.player = loadImage("img/rocket.png");
+    img.planets = [
         loadImage("img/planet0.png"),
         loadImage("img/planet1.png"),
         loadImage("img/planet2.png"),
@@ -17,28 +17,55 @@ function preload() {
         loadImage("img/planet5.png"),
         loadImage("img/planet6.png"),
     ];
-    icons.ufo = loadImage("img/ufo.png");
-    icons.coin = loadImage("img/coin.png");
-    icons.shield = loadImage("img/shield.png");
-    icons.slowmotion = loadImage("img/slowmotion.png");
+    img.ufo = loadImage("img/ufo.png");
+    img.coin = loadImage("img/coin.png");
+    img.shield = loadImage("img/shield.png");
+    img.slowmotion = loadImage("img/slowmotion.png");
 }
 
+let background_y = 0;
+let planet_timer = 0;
 function setup() {
     let c = createCanvas(450, 600);
     c.canvas.addEventListener("contextmenu", e => e.preventDefault());
     c.canvas.style.marginTop = ((window.innerHeight - height) / 2.5) + "px";
-    // put setup code here
-    elements[0] = new Planet(100, 20);
-}
+    stroke(255, 0, 0); strokeWeight(1); noFill(); noSmooth();
 
-function update() {
-    elements[0].update()
+    background_y = height - width * img.background.height / img.background.width;
 }
 
 function draw() {
-    update();
-
-    background(bcolor);
-    // put drawing code here
-    elements[0].draw()
+    // draw background
+    background_y += speed / 2;
+    if (background_y >= 0) {background_y = height - width * img.background.height / img.background.width;}
+    image(img.background, 0, background_y, width, width / img.background.width * img.background.height);
+    // create new planets
+    planet_timer -= 1;
+    if ((planet_timer <= 0) && (random() < planet_chance * speed)) {
+        let r = random([20, 40, 80]);
+        let new_planet = new Planet(random(width), r);
+        if (!collide(new_planet, elements)) {elements.push(new_planet); planet_timer = r * 2 / speed;}
+    }
+    // create new ufos
+    if (random() < ufo_chance * speed) {
+        let new_ufo = new Ufo(...random([
+            [random(width), -20], // top
+            [-20, random(height / 2)], // left
+            [width + 20, random(height / 2)], // right
+        ]));
+        if (!collide(new_ufo, elements)) {elements.push(new_ufo);}
+    }
+    // create new items
+    if (random() < item_chance * speed) {
+        let t = random(["coin", "coin", "coin", "shield", "shield", "slowmotion"]);
+        let new_item = new Item(random(width), t);
+        if (!collide(new_item, elements)) {elements.push(new_item);}
+    }
+    // update elements
+    for (let element of elements) {
+        element.update();
+        element.draw();
+    }
+    // delete elements
+    elements = elements.filter(x => x.visible());
 }
